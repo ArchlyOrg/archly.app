3be717048e04363c2e8dea04fa4d59fb66be1ca6
luxumbra <me@luxumbra.dev>
2022-07-08 14:24:17 +0100

    Disabling the test action and removing unused tests.\n- TODO: get my act together with writing testspnpm test

--- src/__tests__/App.tsx
+++ src/__tests__/App.tsx
@@ -8,13 +8,13 @@ describe('<App />', () => {
     window.history.pushState({}, 'Home', '/')
     renderWithProviders(<App />, false)
 
-    expect(screen.getByText('Loading...')).toBeInTheDocument()
-    await expect(screen.findByText('Apple')).resolves.toBeInTheDocument()
-    await userEvent.click(screen.getByText('Apple'))
+    expect(screen.getByText('Loading')).toBeInTheDocument()
+    await expect(screen.findByText('Archly')).resolves.toBeInTheDocument()
+    await userEvent.click(screen.getByText('Sites'))
 
-    expect(screen.getByText('Loading...')).toBeInTheDocument()
-    await expect(
-      screen.findByText('Vitamins per 100 g (3.5 oz)')
-    ).resolves.toBeInTheDocument()
+    // expect(screen.getByText('Sites')).toBeInTheDocument()
+    // await expect(
+    //   screen.findByText('Vitamins per 100 g (3.5 oz)')
+    // ).resolves.toBeInTheDocument()
   })
 })
